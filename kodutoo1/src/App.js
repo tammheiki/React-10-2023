ee92264fa1de3ad4aff8231bf71822ade7a172dd
tammheiki <148060094+tammheiki@users.noreply.github.com>
2023-10-17 13:03:12 +0300

    kodutoo1
    kodutoo1 17.10.23. heiki

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="nupu-stiil">Vajuta Siia</button>
+      <button className="nupu-stiil-2">Kodutöö</button>
+      <button className="nupu-stiil-3">Roheline</button>
+      <img className="pilt" src="https://estonianworld.com/wp-content/uploads/2023/03/Tallinns-skyline.-Photo-by-Kaupo-Kalda.-1536x1150-1-1024x576.jpg" alt="" />
+      
+
+      
+     
+    </div>
+  );
+}
+
+export default App;
